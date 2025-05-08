51b40ea410855ac28d6b13ecc81ad4b79ca2f1c5
leparan <leparanfelix16@gmail.com>
2025-05-08 11:35:51 +0300

    changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    
+    '''prints fibonacci sequence of length n'''
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    fib = [0, 1]
+    for i in range(2, length):
+        fib.append(fib[i-1] + fib[i-2])
+    print(fib)
     pass
